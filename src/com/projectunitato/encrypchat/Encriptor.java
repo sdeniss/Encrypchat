d796ff5ea2f1b33fec482c6434985929957ce561
sdeniss <sirovdenis@gmail.com>
2014-04-02 18:23:15 +0300

    RSA support

--- src/com/projectunitato/encrypchat/Encriptor.java
+++ src/com/projectunitato/encrypchat/Encriptor.java
@@ -1,22 +1,150 @@
 package com.projectunitato.encrypchat;
 
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.List;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 import android.util.Log;
 
 
 public class Encriptor {
 	
-	
-	String EncriptedMessage;
-	String OriginalMessage;
-	
-	public Encriptor(String message)
+	public static String Encript(String message, KeyPair kp)
 	{
-		//TODO http://www.javamex.com/tutorials/cryptography/rsa_encryption.shtml
-		OriginalMessage = message;
-		EncriptedMessage = "";
-		char c = 'a' + 1;
-		Log.i("mINFO", "ch is " + c);
-		for(int i = 0; i < message.length(); i++)
-			EncriptedMessage += (char) (OriginalMessage.charAt(i) + 1);
+		/*
+		 * TODO
+		 * USE new String(Byte[]) instead of ToString!!!!
+		 */
+		String text = null;
+		try{
+			/*
+			Log.d("Cipher", "Start msg: " + message);
+	        Cipher cipher = Cipher.getInstance("RSA");
+	        cipher.init(Cipher.ENCRYPT_MODE, kp.getPublic());
+	        
+	        text = getStr(cipher.doFinal(getByte(message)));
+	        Log.i("Cipher", "Encrypted msg: " + text);
+	        
+	        cipher.init(Cipher.DECRYPT_MODE, kp.getPrivate());
+			text = new String(cipher.doFinal(text.getBytes()), Charset.forName("UTF-8"));
+	        
+			Log.w("Cipher", "Decrypted msg: " + text);
+			
+			
+			/*
+			for(byte i = -127; i < 127; i++)
+				Log.d("strings", i + "      " + new String(new byte[]{i}, Charset.forName("UTF-8")));
+	 
+	        PublicKey publicKey = kp.getPublic();
+	        PrivateKey privateKey = kp.getPrivate();
+	 
+	        
+	        text = message;
+	        
+	        
+	        Cipher cipher = Cipher.getInstance("RSA");
+	        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+	        byte[] x = cipher.doFinal(text.getBytes());
+	        
+	        String xs = getStr(x);
+	        byte[] xsb = getByte(xs);
+	        
+	        
+	        Log.w("Cipher", x.toString());
+	        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+	        byte[] y = cipher.doFinal(x);
+	        Log.w("Cipher", new String(y));
+	        text = new String(y);
+			*/
+			
+			
+	 
+	        PublicKey publicKey = kp.getPublic();
+	        PrivateKey privateKey = kp.getPrivate();
+	 
+	        text = message;
+	        Cipher cipher = Cipher.getInstance("EAS");
+	        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+	        byte[] x = cipher.doFinal(text.getBytes());
+	 
+	        text = getStr(x);
+	        
+	        
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return text;
 	}
+	
+	
+	public static KeyPair genKey()
+	{
+		
+		try{
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("EAS");
+		keyGen.initialize(2048);
+        KeyPair kp = keyGen.genKeyPair();
+        return kp;
+		}catch(Exception e){
+		
+		}
+		return null;
+	}
+	
+	
+	
+	public static String Decript(String message, KeyPair kp)
+	{
+		Log.d("Cipher", "Decript in: " + message);
+		String text = null;
+		try{
+			PublicKey publicKey = kp.getPublic();
+	        PrivateKey privateKey = kp.getPrivate();
+	 
+	        text = message;
+	        Cipher cipher = Cipher.getInstance("EAS");
+	        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+	        
+	        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+	        byte[] y = cipher.doFinal(getByte(message));
+	        
+	        text = new String(y);
+		}catch(Exception e)
+		{
+		
+		}
+		return text;
+	}
+	
+	private static String getStr(byte[] in)
+	{
+		String str = "";
+		for(int i = 0; i < in.length; i++)
+			str += (char) in[i];
+		return str;
+	}
+	
+	private static byte[] getByte(String str)
+	{
+		byte[] b = new byte[str.length()];
+		for(int i = 0; i < str.length(); i++)
+			b[i] = (byte) str.charAt(i);
+		return b;
+	}
+	
+	
+	
+	
+	
 }
--- src/com/projectunitato/encrypchat/MainActivity.java
+++ src/com/projectunitato/encrypchat/MainActivity.java
@@ -1,27 +1,40 @@
 package com.projectunitato.encrypchat;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
+import java.security.KeyPair;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 
 	
-	EditText et;
-	Button btn;
+	/*
+	 * Both keys will be saved.
+	 * When starting a chat with a new contact, the PublicKey will be shared.
+	 * Both keys are currently one-time generated.
+	 */
 	
+	
+	EditText et;
+	Button btnEncrypt, btnDecrypt;
+	String PRIVATE_KEY_KEY = "rsa_private";
+	String PREF_NAME = "default";
+	KeyPair kp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +42,69 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		et = (EditText) findViewById(R.id.editText1);
-		btn = (Button) findViewById(R.id.button1);
+		SharedPreferences prefs = this.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    String key = prefs.getString(PRIVATE_KEY_KEY, "");
+
+    
+    if (key.equals("")) {
+        // generate KeyPair
+        kp = Encriptor.genKey();
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        ObjectOutputStream o;
+        try {
+            o = new ObjectOutputStream(b);
+            o.writeObject(kp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        byte[] res = b.toByteArray();
+        String encodedKey = Base64.encodeToString(res, Base64.DEFAULT);
+
+        prefs.edit().putString(PRIVATE_KEY_KEY, encodedKey).commit();
+
+    } else {
+        // read the KeyPair from internal storage
+        byte[] res = Base64.decode(key, Base64.DEFAULT);
+        ByteArrayInputStream bi = new ByteArrayInputStream(res);
+        ObjectInputStream oi;
+        try {
+            oi = new ObjectInputStream(bi);
+            Object obj = oi.readObject();
+            kp = (KeyPair) obj;
+            Log.w("Cipher", ((KeyPair) obj).toString());
+        } catch (Exception e)
+        {
+        
+        }
+    }
+
 		
-		btn.setOnClickListener(new View.OnClickListener() {
+		et = (EditText) findViewById(R.id.et_message);
+		btnEncrypt = (Button) findViewById(R.id.button_encrypt);
+		btnDecrypt = (Button) findViewById(R.id.button_decrypt);
+		
+		btnEncrypt.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-			//	MessageSender msgSender = new MessageSender(getApplicationContext());
-			//	msgSender.SendMessage(et.getText().toString());
-				Encriptor encriptor = new Encriptor(et.getText().toString());
+				/*
 				Intent sendIntent = new Intent(Intent.ACTION_VIEW);
 				sendIntent.setData(Uri.parse("sms:"));
-				sendIntent.putExtra("sms_body", encriptor.EncriptedMessage);
+				sendIntent.putExtra("sms_body", Encriptor.Encript(et.getText().toString(), kp));
 				startActivity(sendIntent);
+				
+				*/
+				et.setText(Encriptor.Encript(et.getText().toString(), kp));
+			}
+		});
+		
+		
+		btnDecrypt.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				et.setText(Encriptor.Decript(et.getText().toString(), kp));
 			}
 		});
 	}
